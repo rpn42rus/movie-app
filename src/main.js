9ebb9eb2c578ef5ba8c4e09cf05fb5d5856f5c80
rpn42rus <68055599+rpn42rus@users.noreply.github.com>
2020-10-19 18:15:47 +0700

    инициализация проекта

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import App from "./App.vue";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-// Install BootstrapVue
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false;
 
